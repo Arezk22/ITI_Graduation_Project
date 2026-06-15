345864fc47cced84e539da42b902f1d26947d072
Ahmed Hesham Abdelomnem <147988053+a7mdh4am@users.noreply.github.com>
2026-06-16 01:58:57 +0300

    debug: adding urls checking for debug mode on to make sure serving the files on local machien, change it in production to server using absolute path so apply it in setting.py in production.

--- config/urls.py
+++ config/urls.py
@@ -4,6 +4,8 @@ URL configuration for config project.
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/6.0/topics/http/urls/
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.permissions import AllowAny
@@ -25,3 +27,6 @@ urlpatterns = [
     path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema', permission_classes=[AllowAny]), name='swagger-ui'),
     path('api/redoc/', SpectacularRedocView.as_view(url_name='schema', permission_classes=[AllowAny]), name='redoc'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
