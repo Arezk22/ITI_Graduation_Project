d0c6be1282bea712d90d0c9d427fea13fe640652
Arezk22 <ahmed60401201818966@gmail.com>
2026-06-17 18:43:14 +0300

    account/views.py: Adding EmailLoginView EndPoint and inherite form TokenObtainPairView that using in jwt Auth

--- account/views.py
+++ account/views.py
@@ -1,19 +1,29 @@
-from rest_framework.response import Response
 from rest_framework import status
-from rest_framework.views import APIView
-from rest_framework.authtoken.models import Token
 from rest_framework.permissions import AllowAny
-from .serializers import RegisterSerializer
-# Create your views here.
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework_simplejwt.views import TokenObtainPairView
+
+from .serializers import EmailTokenObtainPairSerializer, RegisterSerializer
+
 
 class RegisterAPIView(APIView):
-    permission_classes=[AllowAny]
-    
-    
-    def post(self,request):
-        serializer=RegisterSerializer(data=request.data)
+    permission_classes = [AllowAny]
+
+    def post(self, request):
+        serializer = RegisterSerializer(data=request.data)
         if serializer.is_valid():
-            serializer.save()
-            return Response({"message":"User created successfully"},status=status.HTTP_201_CREATED)
-        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
-            
+            user = serializer.save()
+            return Response(
+                {
+                    "message": "User created successfully",
+                    "role": user.role,
+                },
+                status=status.HTTP_201_CREATED,
+            )
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class EmailLoginView(TokenObtainPairView):
+    permission_classes = [AllowAny]
+    serializer_class = EmailTokenObtainPairSerializer
