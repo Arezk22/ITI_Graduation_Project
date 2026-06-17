9dafb05592b1b81ef1eef769ce5279ff6e9e4ee7
Arezk22 <ahmed60401201818966@gmail.com>
2026-06-17 04:06:36 +0300

    Account/urls.py: add register url

--- account/urls.py
+++ account/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from rest_framework_simplejwt.views import TokenObtainPairView , TokenRefreshView
+from .views import RegisterAPIView
 urlpatterns = [
     path('api/v1/login',TokenObtainPairView.as_view(),name="login"),
-    path('api/v1/refresh/',TokenRefreshView.as_view(),name='get_refresh')
+    path('api/v1/refresh/',TokenRefreshView.as_view(),name='get_refresh'),
+    # add register url
+    path('api/v1/register/', RegisterAPIView.as_view(), name='register')
 ]
