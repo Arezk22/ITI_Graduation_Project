91a0be55a7aedabd04bdae3dfdb5af7466e75f12
Arezk22 <ahmed60401201818966@gmail.com>
2026-06-17 04:12:21 +0300

    config/setting.py: add corsheaders in middelware and identify port this ui run on it

--- config/settings.py
+++ config/settings.py
@@ -72,8 +72,12 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
+CORS_ALLOW_ALL_ORIGINS = [
+  "http://localhost:5173",
+]
 ROOT_URLCONF = 'config.urls'
 
 TEMPLATES = [
