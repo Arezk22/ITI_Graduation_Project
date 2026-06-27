0777a0c0b33cde708efc6a69407401bf8f0a0da2
Ahmed Hesham Abdelomnem <147988053+a7mdh4am@users.noreply.github.com>
2026-06-27 19:54:09 +0300

    refactor: enhancments

--- account/urls.py
+++ account/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from rest_framework_simplejwt.views import TokenObtainPairView , TokenRefreshView
 urlpatterns = [
     path('api/v1/login',TokenObtainPairView.as_view(),name="login"),
-    path('api/v1/refresh/',TokenRefreshView.as_view(),name='get_refresh')
+    path('api/v1/refresh',TokenRefreshView.as_view(),name='get_refresh')
 ]
