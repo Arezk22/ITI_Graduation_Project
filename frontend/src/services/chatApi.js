6c7fadde39290fff5dc0f3ea321a3183fc4db954
YoussefAdel3 <you272016@gmail.com>
2026-07-03 20:28:13 +0300

    refactor: add celery to gitignore

---
+++ frontend/src/services/chatApi.js
@@ -0,0 +1,22 @@
+import api from "./api";
+
+export const createChat = (tenderId, title) => {
+  return api.post(`/chats/`, { tender_id: tenderId, title });
+};
+
+export const getChats = (tenderId) => {
+  return api.get(`/chats/`, { params: { tender_id: tenderId } });
+};
+
+export const getChatDetails = (chatId) => {
+  return api.get(`/chats/${chatId}/`);
+};
+
+export const deleteChat = (chatId) => {
+  console.log("Deleting chat with ID:", chatId);
+  return api.delete(`/chats/${chatId}/`);
+};
+
+export const sendChatMessage = (chatId, message) => {
+  return api.post(`/chats/${chatId}/messages/`, { message });
+};
